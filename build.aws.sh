5c5010a0228a20b278025866dc52bb4dd84b9c68
Eduard <eduardbargues@gmail.com>
2020-12-28 21:11:59 +0100

    feat: hello-world function deployed to aws

---
+++ build.aws.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#install zip on debian OS, since microsoft/dotnet container doesn't have zip by default
+if [ -f /etc/debian_version ]
+then
+  apt -qq update
+  apt -qq -y install zip
+fi
+
+cd aws
+dotnet restore
+dotnet tool install -g Amazon.Lambda.Tools --framework netcoreapp3.1
+dotnet lambda package --configuration Release --framework netcoreapp3.1 --output-package bin/Release/netcoreapp3.1/hello.zip
+cd ..
---
+++ deploy.aws.sh
@@ -0,0 +1,3 @@
+cd aws
+serverless deploy
+cd ..
